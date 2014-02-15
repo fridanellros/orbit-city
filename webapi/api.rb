5804f5801ca3582418796a57bf9008d6b389d1c7
Patrik Pettersson <pettersson.pa@gmail.com>
2014-02-15 23:58:47 +0100

    Added rack middleware to parse posted json data and fixed returns for api calls
    added more swaps to ignore file.

--- webapi/api.rb
+++ webapi/api.rb
@@ -12,6 +12,26 @@ class Device
     end
 end
 
+class JSonConverter
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    return @app.call(env) unless env['CONTENT_TYPE'] =~ /json/
+    body = env['rack.input'].read ; env['rack.input'].rewind
+    return @app.call(env) unless body && !body.empty?
+    begin
+      parsed = JSON.parse body
+      env.update 'rack.request.form_hash' => parsed, 'rack.input' => env['rack.input']
+    rescue
+    end
+    @app.call(env)
+  end
+end
+
+use JSonConverter
+
 get '/' do
 
 end
@@ -31,48 +51,55 @@ get '/:id' do
 end
 
 get '/learn/:id' do
+  content_type :json
   id = params[:id].to_i
   d = Device.new_device id
-  d.learn
+  d.learn.to_json
 end
 
 get '/switch/:id/:status' do
+    content_type :json
     id = params[:id].to_i
     status = params[:status]
 
     s = Device.new_device id
-    status == 'on' ? s.on : s.off
+    (status == 'on' ? s.on : s.off).to_json
 end
 
 get '/dim/:id/:intensity' do
+    content_type :json
     id = params[:id].to_i
     intensity = params[:intensity].to_f
     lvl = (intensity/100 * 255).to_i
 
     d = Device.new_device id
-    d.dim lvl
+    (d.dim lvl).to_json
 end
 
 get '/name/:id' do
+  content_type :json
   id = params[:id].to_i
   d = Device.new_device id
-  d.name
+  d.name.to_json
 end
 
 post '/name/:id' do
+  content_type :json
   id = params[:id].to_i
   name = params[:name]
   d = Device.new_device id
-  d.name = name
+  (d.name = name).to_json
 end
 
 get '/param/:id/:param' do
+  content_type :json
   d = Device.new_device params[:id].to_i
-  d[params[:param]]
+  d[params[:param]].to_json
 end
 
 post '/param/:id/:param' do
+  content_type :json
   d = Device.new_device params[:id].to_i
-  d[params[:param]] = params[:value]
+  (d[params[:param]] = params[:value]).to_json
 end
 
